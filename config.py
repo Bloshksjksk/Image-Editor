a06da4ec202d4c652e072ebc22284d779da2ea90
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-26 08:07:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
 
     # Enable or disable private bot
     PRIVATE = False
-    CHANNELS =["movie_time_botonly"]
+    CHANNELS =["-1001678093514"]
 
     # Add id without @
     # if PRIVATE is True only users that are in this list are allowed to use the bot
